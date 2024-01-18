c4c1adb958575493d85deb94370dd7f0ea3327b9
cyyoon <cyyoon@cafe24corp.com>
2024-01-18 17:35:33 +0900

    Init Commit

---
+++ modules/virtualserver/variables.tf
@@ -0,0 +1,15 @@
+variable "firewall_label" {}
+variable "linode_token" {}
+variable "linode_instance_image" {}
+variable "linode_instance_type" {}
+variable "linode_instance_tags" {}
+variable "linode_instance_root_pass" {}
+variable "linode_instance_label" {}
+variable "linode_instance_region" {}
+variable "linode_stackscript_id" {}
+variable "linode_virtualserver_firewall_label" {}
+
+
+
+
+
---
+++ test-cyyoon/variables.tf
@@ -0,0 +1,29 @@
+variable "linode_token" {
+    default = "****"
+}
+variable "linode_instance_image" {
+    default = "linode/rocky9"
+}
+variable "linode_instance_type" {
+    default = "g6-nanode-1"
+}
+variable "linode_instance_tags" {
+    default = "sjhan-test-odin-002"
+}
+variable "linode_instance_root_pass" {
+    default = "PlesaeChangeM@"
+}
+variable "linode_instance_hostaneme" {
+    default = "sjhan-test-odin-006"
+}
+variable "linode_stackscript_id" {
+    default = "1297971"
+}
+variable "linode_instance_region" {
+    default = "jp-osa"
+}
+variable "linode_virtualserver_firewall_label" {
+    default = "fwlabel-for-test-cloudservice"
+}
+
+
